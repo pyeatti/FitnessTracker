2e11687f4b5104b922d98db9951cfd250f2f9908
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-11 22:09:49 -0500

    corrected code, deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,15 +1,12 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 let workoutSeed = [
   {
